73343ed978932b002f188642c20629a9b4b2bf04
Don <donmcleanx@gmail.com>
2016-11-25 23:13:56 -0500

    fix broken hmr in dev mode

--- bin/_setup/electron/js/riko.js
+++ bin/_setup/electron/js/riko.js
@@ -1,5 +1,10 @@
 /* eslint-env browser */
 
+//IMPORTANT! THIS IS REQUIRED FOR HOT MODULE REPLACEMENT!
+if(process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept();
+}
+
 import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
