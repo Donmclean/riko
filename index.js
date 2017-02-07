90f707058b6d34d9b8f6dfaf3f855cc1edad39d4
Don <donmcleanx@gmail.com>
2017-02-07 18:09:22 -0500

    add version command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const _v = require('./config/variables');
 
 const command = _v.argv
+    .version()
     .commandDir('cmds')
     .demandCommand(1, 'You need at least one command before moving on')
     .recommendCommands()
