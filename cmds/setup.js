c541b03e74c2c5af8587b6f91db46448c71e19ab
Don <donmcleanx@gmail.com>
2017-02-07 17:17:56 -0500

    clean up

--- cmds/setup.js
+++ cmds/setup.js
@@ -3,15 +3,16 @@ const funcs = require('../config/functions')();
 
 const options = ['js', 'web', 'electron', 'mobile'];
 
-const command = _v.argv
-    .command({
+const command = _v.argv.command(
+    {
         command: 'setup <project-type> <project-name..>',
         aliases: ['setup', 's'],
         desc: 'Creates/Initializes a new project.',
         handler: (argv) => {
             const validChoice = funcs.isValidOption(options, argv.projectType);
+
             if(validChoice) {
-                const projectName = funcs.sanitizeProjectName(argv.projectName)
+                const projectName = funcs.sanitizeProjectName(argv.projectName);
                 funcs.executeSetup(argv._, argv.projectType, projectName);
             } else {
                 funcs.throwOptionsError(options);
