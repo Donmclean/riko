6bfc78727fa159eb17f0fabee28125baa5bf951b
Don <donmcleanx@gmail.com>
2017-05-27 05:56:56 -0400

    conclude prepublish functionality

--- utils/publishScript.js
+++ utils/publishScript.js
@@ -1,98 +1,26 @@
 const
     packageJson = require('../package.json'),
     _v          = require('./variables')(),
-    versionType = process.argv[2],
-    shell = require('./shellRunner');
     readMePath  = _v.path.resolve(__dirname, `../README.md`);
 
-console.log('versionType: ', versionType);
-console.log('shell: ', shell);
+const deferred = _v.Q.defer();
 
-const handleVersionUpdate = (versionUpdateType) => {
-    const versionUpdateTypes = ['patch', 'minor', 'major'];
-    const isValidVersionUpdateType = _v._.includes(versionUpdateTypes, versionUpdateType);
+_v.qfs.read(readMePath)
+    .then((readMe) => {
+        const urlStringStart = 'https://img.shields.io/badge/npm-v';
+        const urlStringEnd = '-blue.svg';
 
-    if(isValidVersionUpdateType) {
-        return _v.spawn('npm', ['version', versionUpdateType], {stdio: 'inherit'});
-    } else {
-        throw new Error('invalid versionUpdateType');
-    }
-};
+        const npmBadgeVersionUrl = readMe.substring(readMe.indexOf(urlStringStart), (readMe.indexOf(urlStringEnd) + urlStringEnd.length));
 
-let spawn = handleVersionUpdate(versionType);
-
-spawn
-    .on('close', () => {
-        return _v.qfs.read(readMePath)
-            .then((readMe) => {
-                const urlStringStart = 'https://img.shields.io/badge/npm-v';
-                const urlStringEnd = '-blue.svg';
-
-                const npmBadgeVersionUrl = readMe.substring(readMe.indexOf(urlStringStart), (readMe.indexOf(urlStringEnd) + urlStringEnd.length));
-
-                if(npmBadgeVersionUrl) {
-                    return _v.qfs.write(readMePath, readMe.replace(npmBadgeVersionUrl, urlStringStart + packageJson.version + urlStringEnd));
-                }
-
-                return null;
-            })
-            .then(() => {
-                shell.series([
-                    'echo "hahahahhaha"',
-                    'echo "ccccc"',
-                    'echo ">>>>>>>>"',
-                ], (err) => {
-                    if (err) {
-                        console.error(`ERROR > ${__filename}`, err);
-                        throw new Error(err);
-                    }
-
-                   console.log('done!!!!!');
-                });
-
-
-                // spawn = spawn
-                //     .on('close', () => {
-                //         return _v.spawn('echo', ['hahhahahahaha'], {stdio: 'inherit'});
-                //     })
-                //     .on('error', (err) => {
-                //         console.error(`ERROR > ${__filename}`, err);
-                //         throw new Error(err);
-                //     });
-                //
-                // spawn = spawn
-                //     .on('close', () => {
-                //         return _v.spawn('echo', ['vvvvvvvvvvv'], {stdio: 'inherit'});
-                //     })
-                //     .on('error', (err) => {
-                //         console.error(`ERROR > ${__filename}`, err);
-                //         throw new Error(err);
-                //     });
-
-                // spawn = spawn
-                //     .on('close', () => {
-                //         return _v.spawn('npm', ['publish'], {stdio: 'inherit'});
-                //     })
-                //     .on('error', (err) => {
-                //         console.error(`ERROR > ${__filename}`, err);
-                //         throw new Error(err);
-                //     });
-                //
-                // spawn = spawn
-                //     .on('close', () => {
-                //         return _v.spawn('git', ['push', '--follow-tags'], {stdio: 'inherit'});
-                //     })
-                //     .on('error', (err) => {
-                //         console.error(`ERROR > ${__filename}`, err);
-                //         throw new Error(err);
-                //     });
-            })
-            .catch((err) => {
-                console.error(`ERROR > ${__filename}`, err);
-                throw new Error(err);
-            });
+        if(npmBadgeVersionUrl) {
+            return _v.qfs.write(readMePath, readMe.replace(npmBadgeVersionUrl, urlStringStart + packageJson.version + urlStringEnd));
+        } else {
+            throw new Error('npmBadgeVersionUrl is invalid');
+        }
     })
-    .on('error', (err) => {
+    .catch((err) => {
         console.error(`ERROR > ${__filename}`, err);
         throw new Error(err);
     });
+
+return deferred.promise;
