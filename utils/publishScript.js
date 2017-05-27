db4a6a3cc2963085eaf1e8b18301f9044c4473a9
Don <donmcleanx@gmail.com>
2017-05-27 06:07:20 -0400

    make prepublish script synchronous

--- utils/publishScript.js
+++ utils/publishScript.js
@@ -3,24 +3,15 @@ const
     _v          = require('./variables')(),
     readMePath  = _v.path.resolve(__dirname, `../README.md`);
 
-const deferred = _v.Q.defer();
+const readMe = _v.fs.readFileSync(readMePath, 'utf8');
 
-_v.qfs.read(readMePath)
-    .then((readMe) => {
-        const urlStringStart = 'https://img.shields.io/badge/npm-v';
-        const urlStringEnd = '-blue.svg';
+const urlStringStart = 'https://img.shields.io/badge/npm-v';
+const urlStringEnd = '-blue.svg';
 
-        const npmBadgeVersionUrl = readMe.substring(readMe.indexOf(urlStringStart), (readMe.indexOf(urlStringEnd) + urlStringEnd.length));
+const npmBadgeVersionUrl = readMe.substring(readMe.indexOf(urlStringStart), (readMe.indexOf(urlStringEnd) + urlStringEnd.length));
 
-        if(npmBadgeVersionUrl) {
-            return _v.qfs.write(readMePath, readMe.replace(npmBadgeVersionUrl, urlStringStart + packageJson.version + urlStringEnd));
-        } else {
-            throw new Error('npmBadgeVersionUrl is invalid');
-        }
-    })
-    .catch((err) => {
-        console.error(`ERROR > ${__filename}`, err);
-        throw new Error(err);
-    });
-
-return deferred.promise;
+if(npmBadgeVersionUrl) {
+    return _v.fs.writeFileSync(readMePath, readMe.replace(npmBadgeVersionUrl, urlStringStart + packageJson.version + urlStringEnd));
+} else {
+    throw new Error('npmBadgeVersionUrl is invalid');
+}
