fdce02c8a87a16a5f17dec13e1039be9fa9acd4c
Don <donmcleanx@gmail.com>
2017-05-27 13:56:01 -0400

    add postversion script

--- utils/publishScript.js
+++ utils/publishScript.js
@@ -1,12 +1,9 @@
 const
-    packageJson         = require('../package.json'),
-    _v                  = require('./variables')(),
-    versionUpdateType   = process.argv,
-    semver              = require('semver'),
-    readMePath          = _v.path.resolve(__dirname, `../README.md`);
+    packageJson = require('../package.json'),
+    _v          = require('./variables')(),
+    readMePath  = _v.path.resolve(__dirname, `../README.md`);
 
 const readMe = _v.fs.readFileSync(readMePath, 'utf8');
-console.log('versionUpdateType: ', versionUpdateType);
 
 const urlStringStart = 'https://img.shields.io/badge/npm-v';
 const urlStringEnd = '-blue.svg';
@@ -14,9 +11,7 @@ const urlStringEnd = '-blue.svg';
 const npmBadgeVersionUrl = readMe.substring(readMe.indexOf(urlStringStart), (readMe.indexOf(urlStringEnd) + urlStringEnd.length));
 
 if(npmBadgeVersionUrl) {
-    _v.fs.writeFileSync(readMePath, readMe.replace(
-        npmBadgeVersionUrl,
-        urlStringStart + semver.inc(packageJson.version) + urlStringEnd));
+    _v.fs.writeFileSync(readMePath, readMe.replace(npmBadgeVersionUrl, urlStringStart + packageJson.version + urlStringEnd));
 } else {
     throw new Error('npmBadgeVersionUrl is invalid');
 }
